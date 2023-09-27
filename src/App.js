7ef10fd5c5751127c8c5486cf22494b087dc1d55
sQuocKhanhs <innespham@gmail.com>
2023-09-27 10:41:17 +0700

    Add CF-6 - Add Signup Page and routing for page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
     </Routes>
   );
 }
---
+++ src/pages/Signin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { logo } from '../assets';
+
+function Signin() {
+  return (
+    <div>
+        <form>
+            <img src={logo} alt='logo'/>
+        </form>
+    </div>
+  )
+}
+
+export default Signin
