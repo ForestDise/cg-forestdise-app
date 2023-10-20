4cda5a88cc2806c5f68873eb1aec7fb4bfa25c44
nguyenvankhanh <vankhanhn735@gmail.com>
2023-10-20 11:55:46 +0700

    update logo wedsite

---
+++ src/api/homeAPI.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const HOME_MANAGEMENT_API = "http://localhost:8080/api";
+
+export const findProducts = async () => {
+  let result = null;
+  try {
+    result = await axios.get(`${HOME_MANAGEMENT_API}/products`);
+  } catch (e) {
+    console.log("Find products API error: " + e);
+  }
+  return result;
+};
